f204c2f12e150d9151bfa21433704bf31e24235d
WasinUddy <wasinsilakong@gmail.com>
2021-01-15 19:49:25 +0700

    backend finale

--- program/backend/analog_output_codegenerator.py
+++ program/backend/analog_output_codegenerator.py
@@ -12,7 +12,7 @@ def analog_output_codegenerator(input_dataframe):
     # defined Empty Starting String
     noncondition_code = """
     
-    """
+"""
 
     # loading table dictionary in to Pandas DataFrame
     input_dataframe_pd = pd.DataFrame.from_dict(input_dataframe)
@@ -30,15 +30,18 @@ def analog_output_codegenerator(input_dataframe):
         # creating Starting Unmodified Function
         unmodified_function = item_dictionary["function_list"]
         print(unmodified_function)
+        header_name = row["name"]
+        header_name = (f"'{header_name}'").replace("'", '"')
 
         # modified unmodified function
         unmodified_function = unmodified_function.replace(item_dictionary["custom id key"], row["INPUT"])
-        unmodified_function = unmodified_function.replace("header", row["name"])
+        unmodified_function = unmodified_function.replace("header", header_name)
         unmodified_function = unmodified_function.replace("value", row["OUTPUT"])
         unmodified_function = unmodified_function.replace("row", row["row"])
 
         # modified function to code
         noncondition_code += unmodified_function
+        noncondition_code += "\n"
 
     return noncondition_code
 

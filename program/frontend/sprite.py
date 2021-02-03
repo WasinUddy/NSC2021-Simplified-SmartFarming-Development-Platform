aae1ae2cca81fcbd48454c53d0509fdbd761bfb2
WasinUddy <wasinsilakong@gmail.com>
2021-02-03 17:54:34 +0700

    sd

--- program/frontend/sprite.py
+++ program/frontend/sprite.py
@@ -132,6 +132,7 @@ class Choice:
         # clear the display box to search
         self.clear = False
 
+
     def draw(self, outline=None):
         # draw box outline
         if outline:
@@ -174,6 +175,17 @@ class Choice:
             if self.x < pos[0] < self.x + self.width*13/10 - 30:
                 for word in range(len(self.display_text) + 1):
                     if self.y + word * self.height - 1 < pos[1] < self.y + self.height + word * self.height + 1:
+                        
+                        self.current_item_mouse_hover = self.display_text[word - 1]
+                        self.temp_mouse_hover.append(self.current_item_mouse_hover)
+                        if len(self.temp_mouse_hover) >= 1:
+                            if self.temp_mouse_hover[-1] == self.temp_mouse_hover[-2]:
+                                self.temp_mouse_hover.append(self.current_item_mouse_hover)
+                                if len(self.temp_mouse_hover) == self.waiting_time_n_var:
+                                    "Draw Description"
+                                    
+                            else:
+                                self.temp_mouse_hover = []
                         if word > 0 and clicked:
                             # get the result
                             self.default = self.display_text[word - 1]
@@ -656,26 +668,25 @@ class Infobox:
         # word
         self.text = text
         self.use_list = []
-        self.txt_surface = font.render(text, 1, self.color)
         self.font_size = 20
         # default text
         self.active = True
         self.top, self.bottom = 0, (h-10)// self.font_size
+        self.surface = None
 
 
     # draw object on screen
     def draw(self, screen):
-        if active:
+        self.surface = screen
+        if self.active:
             font = pg.font.SysFont(FONTNAME, self.font_size)
             self.use_list = self.text[self.top:self.bottom]
-            pg.draw.rect(self.surface, self.grey,self.rect)
+            pg.draw.rect(self.surface, grey,self.rect)
             for row in range(len(self.use_list)):
-                for col in range(len(self.use_list[row])):
-                    text = font.render(str(self.use_list[row][col]), 1, self.color)
-                    self.surface.blit(text,
-                                        (self.rect.x + (self.rect.w - text.get_width()) / 2,
-                                        self.rect.y + (self.rect./self.bottom - text.get_height()) / 2))
-            scrollbar(None)
+                text = font.render(str(self.use_list[row]), 1, self.color)
+                self.surface.blit(text,(self.rect.x + (self.rect.w - text.get_width()) / 2,
+                                        self.rect.y + 20 *row + (self.rect.h/self.bottom - text.get_height()) / 2))
+            self.scrollbar(None)
             #pg.draw.rect(screen, self.color, self.rect, 2)
 
      # create scrollbar on table
@@ -684,16 +695,15 @@ class Infobox:
             pg.draw.rect(self.surface, outline,
                              (self.x + total_width + total_gap-2, self.y-2, 14,
                               4 + self.rect.h * min(1 + len(self.table['items']), self.bottom - self.top + 1)))
-        pg.draw.rect(self.surface, table_header, (self.rect.x + self.rect.w*13/10 - 29, self.rect.y + self.rect.h, 10, self.rect.h *
-                                                  min(len(self.text), len(self.use_list))))
-        pg.draw.rect(self.surface, grey2, (self.rect.x + self.rect.w*13/10 - 29, self.rect.y + 0.5 + self.rect.h *
+        pg.draw.rect(self.surface, table_header, (self.rect.x + self.rect.w*13/10 - 29, self.rect.y, 10, self.rect.h ))
+        pg.draw.rect(self.surface, grey2, (self.rect.x + self.rect.w*13/10 - 29, self.rect.y + 20 *
                                            (((self.bottom - self.top) / (
-                                                   len(self.test) - self.bottom + self.top + 1)) * self.top + 1), 10,
-                                           0.5 + self.rect.h * ((self.boxes - self.top) / (
+                                                   len(self.text) - self.bottom + self.top)) * self.top), 10,
+                                           20 * ((self.bottom - self.top) / (
                                                    len(self.text) - self.bottom + self.top + 1))))
                                 
     def handle_event(self, event):
-        if active:
+        if self.active:
             # mouse function
             if event.type == pg.MOUSEBUTTONDOWN:
                 # mouse wheel scroll down
@@ -703,7 +713,7 @@ class Infobox:
                         self.bottom -= 1
                 # mouse wheel scroll up
                 elif event.button == 5:
-                    if self.boxes < len(self.text):
+                    if self.bottom < len(self.text):
                         self.top += 1
                         self.bottom += 1
                 
